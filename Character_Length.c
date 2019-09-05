cf356bcf2f1b476e171ca63e45f9756e926ce936
Imran Hoshain <princ.imran@gmail.com>
2019-09-06 00:33:09 +0600

    Print How many character you input

---
+++ Character_Length.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+char text[200];
+printf("Enter your text: ");
+gets(text);
+int i, length=0;
+for(i=0;text[i]!='\0';i++)
+    length++;
+printf("\nYour text length is: %d",length);
+
+
+}
